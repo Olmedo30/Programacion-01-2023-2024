c108eb82deeaa56ef5dbb0974c71e93ffba9b492
Olmedo30 <160176419+Olmedo30@users.noreply.github.com>
2024-09-11 16:26:17 +0200

    Create Ejericio2.java

---
+++ EjerciciosRepaso/Ejericio2.java
@@ -0,0 +1,19 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class ej2 {
+
+	public static void main(String[] args) {
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce el valor del número 'a' (Siendo la ecucación a^2+bx+ = 0)");
+		a = sc.nextInt();
+		System.out.println("Introduce el valor del número 'b' (Siendo la ecucación a^2+bx+ = 0)");
+		b = sc.nextInt();
+		System.out.println("Introduce el valor del número 'c' (Siendo la ecucación a^2+bx+ = 0)");
+		c = sc.nextInt();
+	}
+}
