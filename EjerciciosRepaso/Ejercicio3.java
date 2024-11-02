1ee25f562b29decd9431e837fa178bd5cf12d739
Olmedo30 <160176419+Olmedo30@users.noreply.github.com>
2024-11-02 20:34:54 +0100

    Create Ejercicio3.java

---
+++ EjerciciosRepaso/Ejercicio3.java
@@ -0,0 +1,17 @@
+/*
+ * Realiza un programa que imprima los números del 1 al 10 utilizando un bucle for.
+ */
+package Ejercicios;
+
+/**
+ * @author Álvaro Olmedo Fernández
+ */
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        
+        //Realizamos el bucle donde irá sumando de 1 en 1 y a su vez mostrándolo por pantalla hasta llegar a 10.
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
