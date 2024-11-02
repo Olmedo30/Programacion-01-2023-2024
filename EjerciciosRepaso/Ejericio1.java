bcd059c83d732f27cfbbb7692d6e9dbcd2b77b34
Olmedo30 <160176419+Olmedo30@users.noreply.github.com>
2024-11-02 20:33:26 +0100

    Update Ejericio1.java

--- EjerciciosRepaso/Ejericio1.java
+++ EjerciciosRepaso/Ejericio1.java
@@ -2,17 +2,27 @@ package Ejercicios;
 
 import java.util.Scanner;
 
-public class ej1 {
-
-	public static void main(String[] args) {
-		double kilometres = 0;
-		double milles = 0;
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Introduce el número de milla que quieres pasar a kilómetros: ");
-		milles = sc.nextDouble();
-		kilometres = ( milles * 1852 ) / 1000;
-		System.out.println(kilometres);
-	}
-	
+/**
+ * @author Álvaro Olmedo Fernández
+ */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        
+        //Creamos el objeto scanner par leer lo que se ponga el teclado.
+        Scanner sc = new Scanner(System.in);
+        
+        //Pedimos el primer número de la suma.
+        System.out.println("Dime el primer número de la suma:");
+        int numero1 = sc.nextInt();
+        
+        //Pedimos el segundo número de la suma.
+        System.out.println("Dime el segundo número de la suma:");
+        int numero2 = sc.nextInt();
+        
+        //Realizamos la suma de los dos números.
+        int suma = numero1 + numero2;
+        
+        //Imprimimos la suma del total de los dos números.
+        System.out.println("La suma total de los dos números es: " + suma);
+    }
 }
