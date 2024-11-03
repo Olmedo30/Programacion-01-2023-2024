d4b1d770d10561db3820f75817f2cd464e622cf2
Olmedo30 <160176419+Olmedo30@users.noreply.github.com>
2024-11-03 21:33:47 +0100

    Update HolaMundo.java

--- Ejemplos/HolaMundo.java
+++ Ejemplos/HolaMundo.java
@@ -1,7 +1,6 @@
 package HolaMundo;
-
+//Primer programa antes de empezar en este mundo de la programación en Java
 public class HolaMundo {
-
 	public static void main(String[] args) {
 		String hola;
 	}
